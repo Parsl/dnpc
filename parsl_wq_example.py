2c3b79a88944272895e22c23a991eebdc18f2828
Ben Clifford <benc@hawaga.org.uk>
2022-07-26 12:32:08 +0000

    Move schema creation into separate script

--- parsl_wq_example.py
+++ parsl_wq_example.py
@@ -19,45 +19,7 @@ def init_sql() -> sqlite3.Connection:
                          detect_types=sqlite3.PARSE_DECLTYPES |
                          sqlite3.PARSE_COLNAMES)
 
-    create_tables(db)
-
     return db
-
-
-def create_tables(db: sqlite3.Connection) -> None:
-
-    # All spans have a UUID that identifies them without
-    # a containing superspan to name them.
-
-    cursor = db.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS span ("
-                   "uuid TEXT PRIMARY KEY,"
-                   "type TEXT NOT NULL,"  # domain style
-                   "note TEXT"
-                   ")")
-
-    # each event exists within exactly one span
-    # it is identified by a UUID
-    cursor.execute("CREATE TABLE IF NOT EXISTS event ("
-                   "uuid TEXT PRIMARY KEY,"
-                   "span_uuid TEXT REFERENCES span (uuid),"
-                   "time TEXT NOT NULL,"
-                   "type TEXT NOT NULL,"  # meaning comes from span.type
-                   "note TEXT"
-                   ")")
-
-    # spans can be contained, DAG-style, within other spans.
-    # the naming of subspans is a bit complicated - the name
-    # should make sense within the parent span, and so a
-    # subspan may have different names depending on which
-    # superspan it is being viewed from.
-
-    cursor.execute("CREATE TABLE IF NOT EXISTS subspan ("
-                   "superspan_uuid TEXT REFERENCES span(uuid),"
-                   "subspan_uuid TEXT REFERENCES span (uuid),"
-                   "key TEXT NOT NULL,"
-                   "note TEXT"
-                   ")")
 
 if __name__ == "__main__":
     main()
