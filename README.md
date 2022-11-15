40b165b17add32d1e0ce14caafc0a178f0b8358a
Ben Clifford <benc@hawaga.org.uk>
2022-11-15 02:32:34 -0800

    Add 2d kde heatmap to twoplots

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ conda create --name dnpcsql-analysis python=3.10
 conda activate dnpcsql-analysis
 pip3 install matplotlib
 pip3 install sqlalchemy
-
+pip3 install scipy
--- task_end_latency/twoevents.py
+++ task_end_latency/twoevents.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats
 import sqlalchemy
 import sqlite3
 
 def plot(query, output_filename):
+    xdata, ydata = get_data(query)
+    plot_by_time(xdata, ydata, output_filename)
+    plot_kde(xdata, ydata, "kde-"+output_filename)
+    plot_histo(ydata, "histo-"+output_filename)
+
+def get_data(query):
     db_name = "./dnpc.sqlite3"
     db = sqlite3.connect(db_name,
                      detect_types=sqlite3.PARSE_DECLTYPES |
@@ -21,6 +28,11 @@ def plot(query, output_filename):
 
     ydata = np.array([float(y) for (x,y) in rows])
 
+    return xdata, ydata
+
+
+def plot_by_time(xdata, ydata, output_filename):
+
     fig, ax = plt.subplots()
 
     # ax.hist(durations_running_ended_exec_done, bins=100, color="#0000FF")
@@ -31,3 +43,35 @@ def plot(query, output_filename):
 
     plt.savefig(output_filename)
 
+def plot_histo(ydata, output_filename):
+
+    fig, ax = plt.subplots()
+
+    ax.hist(ydata, bins=100, color="#0000FF")
+    ax.axvline(ydata.mean(), linestyle='dashed', linewidth=1, label="mean", color = "#009900")
+    ax.axvline(np.percentile(ydata, 50), linestyle='dashed', linewidth=1, label="median", color = "#FF00AA")
+    ax.axvline(np.percentile(ydata, 95), linestyle='dashed', linewidth=1, label="95%", color = "#AA0000")
+    ax.axvline(np.percentile(ydata, 100), linestyle='dashed', linewidth=1, label="maximum", color = "#FF4400")
+    ax.legend()
+
+    plt.xlabel("seconds between start,end")
+    plt.ylabel("number of tasks in bin")
+
+    plt.savefig(output_filename)
+
+def plot_kde(xdata, ydata, output_filename):
+    """
+    Credit to:
+    https://www.python-graph-gallery.com/85-density-plot-with-matplotlib
+    """
+    print("calc kde")
+    k = scipy.stats.kde.gaussian_kde([xdata,ydata])
+    nbins=300
+    print("generate grid")
+    xi, yi = np.mgrid[xdata.min():xdata.max():nbins*1j, ydata.min():ydata.max():nbins*1j]
+    print("flatten")
+    zi = k(np.vstack([xi.flatten(), yi.flatten()]))
+    print("plot colourmesh")
+    plt.pcolormesh(xi, yi, zi.reshape(xi.shape), shading='auto')
+    print("save")
+    plt.savefig(output_filename)
