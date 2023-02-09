bad6f7e3ffe53736a1b5f436ad292c1d828a7926
Ben Clifford <benc@hawaga.org.uk>
2023-02-09 15:38:46 +0000

    Plot parsl task duration histogram

--- parsl_wq_example.py
+++ parsl_wq_example.py
@@ -1,6 +1,7 @@
 import sqlite3
 import dnpcsql.parsl
 import dnpcsql.workqueue
+import dnpcsql.twoevents as twoev
 from dnpcsql.schema import create_tables
 
 def main() -> None:
@@ -14,6 +15,20 @@ def main() -> None:
 
     connection.commit()
     connection.close()
+
+    # now plot parsl-level task durations
+
+    parsl_task_invoked_to_returned = """
+     SELECT event_start.time,
+            event_end.time - event_start.time
+       FROM event as event_start,
+            event as event_end
+      WHERE event_start.span_uuid = event_end.span_uuid
+        and event_start.type = "invoked"
+        and event_end.type="returned";
+    """
+
+    twoev.plot(parsl_task_invoked_to_returned, "parsl-task-durations")
 
 
 def init_sql() -> sqlite3.Connection:
