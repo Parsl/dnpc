e59ef91a8213404045cd9ea8ed97acfa29af7973
Ben Clifford <benc@hawaga.org.uk>
2023-02-13 16:45:14 +0000

    Remove unused comments which documented my query experimentation

--- dnpcsql/list_event_sequences.py
+++ dnpcsql/list_event_sequences.py
@@ -5,22 +5,6 @@
 import itertools
 import sqlite3
 
-# this will select all events directly on parsl tasks:
-# select event.uuid, event.type, event.time from event, span where event.span_uuid = span.uuid and span.type = "parsl.task";
-#
-# or hopefully equivalently:
-# select event.uuid, event.type, event.time from event, span where event.span_uuid = span.uuid and span.uuid in (select span.uuid from span where span.type="parsl.task");
-
-
-# this does things recursively:
-# with recursive descs(span_uuid) as (select span.uuid from span where span.type="parsl.task" union select subspan.subspan_uuid from subspan, descs where subspan.superspan_uuid = descs.span_uuid) select count(span_uuid) from descs;
-
-# more playing: records a root span ID - which in this case is always the root parsl task of a tree
-# with recursive descs(root_span_uuid, span_uuid) as (select span.uuid, span.uuid from span where span.type="parsl.task" union select descs.root_span_uuid, subspan.subspan_uuid from subspan, descs where subspan.superspan_uuid = descs.span_uuid) select span.uuid, span.type, event.type from descs, span, event where span.uuid = root_span_uuid and event.span_uuid = span.uuid order by root_span_uuid;
-
-# this will return root span uuid (i.e. each parsl task span uuid) with all events in tree under it: event time, span type and event type
-# with recursive descs(root_span_uuid, span_uuid) as (select span.uuid, span.uuid from span where span.type="parsl.task" union select descs.root_span_uuid, subspan.subspan_uuid from subspan, descs where subspan.superspan_uuid = descs.span_uuid) select descs.root_span_uuid, event.time, span.type, event.type from descs, span, event where span.uuid = descs.span_uuid and event.span_uuid = span.uuid order by root_span_uuid, event.time;
-
 if __name__ == "__main__":
 
     query = """
