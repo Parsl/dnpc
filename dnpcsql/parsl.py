cf33b554099c5798644a57de292c58fe50bfe814
Ben Clifford <benc@hawaga.org.uk>
2023-03-06 13:31:19 +0000

    Import interchange task view, without binding to parsl-level executor tasks

--- dnpcsql/parsl.py
+++ dnpcsql/parsl.py
@@ -256,6 +256,83 @@ def import_monitoring_db(dnpc_db, monitoring_db_name):
 
             dnpc_db.commit()
 
+        # look for htex logs
+        # right now using a hard-coded executor name
+        # but could do something else like look in all directories
+        # If funcx is going to be using parsl htex without a DFK around it,
+        # which is likely, then this code should be able to import htex logs
+        # without a surrounding parsl workflow/parsl tasks.
+        # (but for example be able to bind to however htex will identify
+        # it's submitted tasks one layer up - probably with a uuid)
+        # That doesn't need to happen inside the parsl importer, but suggests
+        # that the htex importer should be a separate module.
+
+        htex_task_to_uuid = {}
+
+        executor_label = "htex_Local"
+
+        htex_interchange_filename = f"{rundir}/{executor_label}/interchange.log"
+        print(f"looking for: {htex_interchange_filename}")
+        if os.path.exists(htex_interchange_filename):
+            # Interchange reports these two log lines which are relevant for
+            # task status:
+            # 2023-03-06 11:20:07.190 interchange:485 HTEX-Interchange(18277) MainThread process_tasks_to_send [DEBUG]  Sent tasks: [1] to manager b'4bf9bf8c1848'
+            re_interchange_task_to_manager = re.compile('(.*) interchange:.* Sent tasks: \[(.*)\] to manager.*$')
+            # 2023-03-06 11:20:08.040 interchange:533 HTEX-Interchange(18277) MainThread process_results_incoming [DEBUG]  Removing task 3 from manager record b'4bf9bf8c1848'
+            re_interchange_removing_task =   re.compile('(.*) interchange:.* Removing task ([0-9]+) .*$')
+            with open(htex_interchange_filename, "r") as f:
+                for log_line in f.readlines():
+                    # for parsing a logfile timestamp:
+                    # event.time = datetime.datetime.strptime(timestamp + "." + fractime, "%Y-%m-%d %H:%M:%S.%f").timestamp()
+                    m = re_interchange_task_to_manager.match(log_line)
+                    if m:
+                        event_time = datetime.datetime.strptime(m[1], "%Y-%m-%d %H:%M:%S.%f").timestamp()
+                        tasklist = m[2]
+                        # this assumes that there's only one task in the task list
+                        # which won't work except in serialised use cases - an exception
+                        # will be raised here and you can read this comment and implement
+                        # a suitable parse/for loop or change the interchange log
+                        # message.
+                        task_id = int(tasklist)
+
+                        if task_id not in htex_task_to_uuid:
+                            htex_task_span_uuid = str(uuid.uuid4())
+                            htex_task_to_uuid[task_id] = htex_task_span_uuid
+                            dnpc_cursor.execute("INSERT INTO span (uuid, type, note) VALUES (?, ?, ?)", (htex_task_span_uuid, 'parsl.executor.htex.task', 'from interchange.log'))
+                        else:
+                            htex_task_span_uuid = htex_task_to_uuid[task_id]
+
+                        event_uuid = str(uuid.uuid4())
+                        event_type = "interchange_to_manager"
+                        dnpc_cursor.execute("INSERT INTO event (uuid, span_uuid, time, type, note) VALUES (?, ?, ?, ?, ?)", (event_uuid, htex_task_span_uuid, event_time, event_type, 'from interchange.log'))
+
+                    m = re_interchange_removing_task.match(log_line)
+                    if m:
+                        event_time = datetime.datetime.strptime(m[1], "%Y-%m-%d %H:%M:%S.%f").timestamp()
+                        task_id = int(m[2])
+
+                        if task_id not in htex_task_to_uuid:
+                            htex_task_span_uuid = str(uuid.uuid4())
+                            htex_task_to_uuid[task_id] = htex_task_span_uuid
+                            dnpc_cursor.execute("INSERT INTO span (uuid, type, note) VALUES (?, ?, ?)", (htex_task_span_uuid, 'parsl.executor.htex.task', 'from interchange.log'))
+                        else:
+                            htex_task_span_uuid = htex_task_to_uuid[task_id]
+
+                        event_uuid = str(uuid.uuid4())
+                        event_type = "interchange_removing_task"
+                        dnpc_cursor.execute("INSERT INTO event (uuid, span_uuid, time, type, note) VALUES (?, ?, ?, ?, ?)", (event_uuid, htex_task_span_uuid, event_time, event_type, 'from interchange.log'))
+
+        # next, manager logs
+        # 2023-03-06 11:20:07.190 parsl:304 18294 Task-Puller [DEBUG]  Got executor tasks: [1], cumulative count of tasks: 1
+        # manager does not log the identity of task results - because it never unpacks them? not sure why that's different than the task send path?
+
+        # next, worker logs
+        # 2023-03-06 11:20:17.249 worker_log:597 18304 MainThread [INFO]  Received executor task 41
+        # 2023-03-06 11:20:17.282 worker_log:615 18304 MainThread [INFO]  Completed executor task 41
+        # 2023-03-06 11:20:17.282 worker_log:626 18304 MainThread [INFO]  All processing finished for executor task 41
+
+        dnpc_db.commit()
+
         # Now import pickled event stats from parsl_tracing.pickle which is
         # a DESC-branch specific development.
         # Right now there isn't enough info to tie such a pickle file into
