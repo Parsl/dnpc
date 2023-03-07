b967cacdee4ce047679d2809edae824dd2478145
Ben Clifford <benc@hawaga.org.uk>
2023-03-07 11:01:05 +0000

    Add type annotations to store_event

--- dnpcsql/importerlib.py
+++ dnpcsql/importerlib.py
@@ -1,6 +1,10 @@
 import uuid
 
-def store_event(*, cursor, span_uuid, event_time, event_type, description):
+def store_event(*, cursor,
+                   span_uuid: str,
+                   event_time: float,
+                   event_type: str,
+                   description: str):
     """writes an event into the database"""
 
     event_uuid = str(uuid.uuid4())
