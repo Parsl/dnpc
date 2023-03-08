4600c6cd19d40c85c9e7dad6c3608972ceb94ca3
Ben Clifford <benc@hawaga.org.uk>
2023-03-08 12:49:33 +0000

    bind Work Queue tasks to containing worker

--- dnpcsql/workqueue.py
+++ dnpcsql/workqueue.py
@@ -24,7 +24,7 @@ def import_all(db: sqlite3.Connection, transaction_log_path) -> Dict[str, str]:
     # moment, but this will need to happen somehow to eg tie into the
     # relevant parsl-level task spans)
 
-    task_re = re.compile('([0-9]+) [0-9]+ TASK ([0-9]+) ([^ ]+) .*')
+    task_re = re.compile('([0-9]+) [0-9]+ TASK ([0-9]+) ([^ ]+) ([^ ]+).*')
 
     # time manager_pid TRANSFER (INPUT|OUTPUT) taskid cache_flag sizeinmb walltime filename
     transfer_re = re.compile('([0-9]+) [0-9]+ TRANSFER ([^ ]+) ([0-9]+) ([^ ]+) .*')
@@ -32,6 +32,8 @@ def import_all(db: sqlite3.Connection, transaction_log_path) -> Dict[str, str]:
     task_to_span_map: Dict[str, str] = {}
 
     cursor = db.cursor()
+
+    worker_address_to_span_map: Dict[str, str] = {}
 
     with open(transaction_log_path, "r") as logfile:
         for line in logfile:
@@ -45,7 +47,7 @@ def import_all(db: sqlite3.Connection, transaction_log_path) -> Dict[str, str]:
 
                 wq_task_id = m[2]
 
-                span_id = local_key_to_span_uuid(
+                task_span_uuid = local_key_to_span_uuid(
                     cursor = cursor,
                     local_key = wq_task_id,
                     namespace = task_to_span_map,
@@ -55,11 +57,24 @@ def import_all(db: sqlite3.Connection, transaction_log_path) -> Dict[str, str]:
                 unix_time = float(m[1]) / 1000000.0
 
                 store_event(cursor=cursor,
-                            span_uuid=span_id,
+                            span_uuid=task_span_uuid,
                             event_time=unix_time,
                             event_type=m[3],
                             description='Event from transaction_log'
                            )
+
+                if m[3] in ["RUNNING", "WAITING_RETRIEVAL"]:  # we can capture worker ID
+                    worker_address = m[4]
+                    worker_span_uuid = local_key_to_span_uuid(
+                        cursor = cursor,
+                        local_key = worker_address,
+                        namespace = worker_address_to_span_map,
+                        span_type = 'workqueue.worker',
+                        description = 'Work Queue WORKER from transaction_log')
+
+                    # TODO: don't need to do this on both RUNNING and WAITING_RETRIEVAL...
+                    cursor.execute("INSERT INTO subspan (superspan_uuid, subspan_uuid, key) VALUES (?, ?, ?)", (worker_span_uuid, task_span_uuid, wq_task_id))
+
             m = transfer_re.match(line)
             if m:
                 wq_task_id = m[3]
