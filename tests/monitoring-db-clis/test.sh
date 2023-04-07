ec461afe9eac91ea5b7ca19639c646dbe2f53c86
Ben Clifford <benc@hawaga.org.uk>
2023-04-07 11:23:46 +0000

    Add basic CI test for graphics-output CLIs

--- tests/monitoring-db-clis/test.sh
+++ tests/monitoring-db-clis/test.sh
@@ -2,12 +2,21 @@
 
 rm -f dnpc.sqlite3 *.out
 
+# Check importer runs, but do not check stdout
+# The sqlite3 output will be checked indirectly by
+# subsequent "identical output" CLI tool runs.
 python3 -m dnpcsql.import_parsl_runinfo ./runinfo
 
+# check these command line tools produce expected output
 python3 -m dnpcsql.list_span_types > list_span_types.out
 diff list_span_types.out list_span_types.out.expected
 
 python3 -m dnpcsql.list_event_sequences > list_event_sequences.out
 diff list_event_sequences.out list_event_sequences.out.expected
+
+# check these tools run, but do not check output makes sense
+python3 -m dnpcsql.dag_span_types
+python3 -m dnpcsql.plot_task_durations
+python3 -m dnpcsql.plot_spans_in_each_state
 
 echo Test completed successfully
